4a4c4a48b4404e1f558484a519c209646f0b6ce4
Piyapan Rodkuen <piyapannr@gmail.com>
2019-01-23 12:25:03 +0700

    added: term and related config

--- config/starter-kit.php
+++ config/starter-kit.php
@@ -1,5 +1,8 @@
 <?php
 
 return [
-    //
+  'term_model' => Raystech\StarterKit\Models\Term::class,
+  'term_meta_model' => Raystech\StarterKit\Models\TermMeta::class,
+  'term_relationship_model' => Raystech\StarterKit\Models\TermRelationship::class,
+  'term_taxonomy_model' => Raystech\StarterKit\Models\TermTaxonomy::class
 ];
