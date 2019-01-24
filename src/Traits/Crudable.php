1a8261ebeeb46d0bdf4f43ccfe28445e8662f974
Piyapan Rodkuen <piyapannr@gmail.com>
2019-01-24 15:35:19 +0700

    added: crudable trait

---
+++ src/Traits/Crudable.php
@@ -0,0 +1,14 @@
+<?php 
+
+namespace Raystech\StarterKit\Traits;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Database\Query\JoinClause;
+
+trait Crudable
+{
+
+}
