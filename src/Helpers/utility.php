1080227f90bcb272dc188bfa6496b93cc6b2fee9
Piyapan Rodkuen <piyapannr@gmail.com>
2019-01-28 14:18:31 +0700

    added: check route helper

--- src/Helpers/utility.php
+++ src/Helpers/utility.php
@@ -1,5 +1,22 @@
 <?php
 
+if (!function_exists('menuIsRoute')) {
+  function menuIsRoute($route, $active_class = 'active') {
+    $curr_route = Route::currentRouteName();
+    if($route == $curr_route) {
+      return $active_class;
+    }
+  }
+}
+
+if (!function_exists('urlDoesContainParam')) {
+  function urlDoesContainParam($param_name, $param_value = true, $active_class = 'active') {
+    if(request()->get($param_name) == $param_value) {
+      return $active_class;
+    }
+  }
+}
+
 /**
  * prints array in readable format
  *
