ce6992d93b3c4a074b219fdafef43556126f44ab
Piyapan Rodkuen <piyapannr@gmail.com>
2019-02-16 15:32:48 +0700

    update: summer note cdn

--- src/Resources/views/posts/edit.blade.php
+++ src/Resources/views/posts/edit.blade.php
@@ -111,8 +111,8 @@
 @section('scripts')
 
 <!-- include summernote css/js -->
-<link href="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.11/summernote.css" rel="stylesheet">
-<script src="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.11/summernote.js"></script>
+<link href="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.11/summernote-bs4.css" rel="stylesheet">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.11/summernote-bs4.js"></script>
 
 <script type="text/javascript">
   $(function() {
